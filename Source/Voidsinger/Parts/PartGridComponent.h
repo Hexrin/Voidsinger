67690bb211402f1f96d7ab3604524042462acd78
Liam Healey <snickerscattoruletheworld@gmail.com>
2021-08-26 18:24:14 -0600

    Im struggleing alreay

--- Source/Voidsinger/Parts/PartGridComponent.h
+++ Source/Voidsinger/Parts/PartGridComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "BasePart.h"
 #include "PartGridComponent.generated.h"
 
 
@@ -19,10 +20,11 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	TArray<TArray<UBasePart>> PartGrid;
 		
 };
