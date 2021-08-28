6a3e3bd6e746135e3472ff716658ed06b0dd88c1
Hexrin <71273371+Hexrin@users.noreply.github.com>
2021-08-28 17:29:27 -0600

    oh god please commit this too i don't want to lose this

---
+++ Source/Voidsinger/Ships/ShipMovementComponent.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BaseShip.h"
+#include "ShipMovementComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VOIDSINGER_API UShipMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+
+	UShipMovementComponent();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void AddForce(FVector2D ForceLocation, FVector2D Force);
+	
+private:
+
+	class ABaseShip* Ship;
+	FVector2D Velocity;
+	float AngularVelocity;
+
+};
