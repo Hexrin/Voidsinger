a853e5b78cc275a019cb389cfb3225e93e1cb80d
Liam Healey <snickerscattoruletheworld@gmail.com>
2021-09-08 20:19:54 -0600

    conectivity detection working

--- Source/Voidsinger/FunctionLibrary.cpp
+++ Source/Voidsinger/FunctionLibrary.cpp
@@ -39,7 +39,7 @@ bool UFunctionLibrary::PointsConnected(TMap<FIntPoint, UBasePart*> PartGrid, FIn
 		return true;
 	}
 	
-
+	ConnectivityArray.Emplace(StartPoint);
 
 	bool ReturnValue = false;
 
@@ -48,13 +48,12 @@ bool UFunctionLibrary::PointsConnected(TMap<FIntPoint, UBasePart*> PartGrid, FIn
 	bool YIsPosive = (EndPoint - StartPoint).Y > 0;
 	UE_LOG(LogTemp, Warning, TEXT("Direction x=%i, y=%i"), (EndPoint - StartPoint).X, (EndPoint - StartPoint).Y);
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 4; i++)
 	{
 		FIntPoint TargetPoint = (!IsXCloser ^ (i % 2 == 1)) ? FIntPoint((XIsPosive ^ (i > 1)) ? 1 : -1, 0) : FIntPoint(0, (YIsPosive ^ (i > 1)) ? 1 : -1);
 		UE_LOG(LogTemp, Warning, TEXT("Target Point x=%i, y=%i, Xclose=%i, Xpos=%i, Ypos=%i"), TargetPoint.X, TargetPoint.Y, (IsXCloser ^ (i % 2 == 1)) ? 1 : 0, !(XIsPosive ^ (i > 1)) ? 1 : 0, !(YIsPosive ^ (i > 1)) ? 1 : 0);
 		if (!ConnectivityArray.Contains(StartPoint + TargetPoint) && PartGrid.Contains(StartPoint + TargetPoint))
 		{
-			ConnectivityArray.Emplace(StartPoint + TargetPoint);
 			ReturnValue = PointsConnected(PartGrid, StartPoint + TargetPoint, EndPoint, ConnectivityArray);
 			if (ReturnValue)
 			{
