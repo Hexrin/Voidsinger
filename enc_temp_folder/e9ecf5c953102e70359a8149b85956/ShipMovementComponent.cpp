059e934d99c6991080d8377569a97aaaf61b6c9c
Liam <56327428+LiamDHealey@users.noreply.github.com>
2021-09-14 09:48:43 -0600

    WOOOO IT workz

--- enc_temp_folder/e9ecf5c953102e70359a8149b85956/ShipMovementComponent.cpp
+++ enc_temp_folder/e9ecf5c953102e70359a8149b85956/ShipMovementComponent.cpp
@@ -57,7 +57,7 @@ void UShipMovementComponent::AddForce(FVector2D RelativeForceLocation, FVector2D
 	{
 		float VelocityShare = RelativeForce.Size();
 		VelocityShare = VelocityShare / (VelocityShare + DeltaVelocity.Size());
-		//DrawDebugLine(GetWorld(), GetOwner()->GetActorLocation() + FVector(RelativeForceLocation, 0), (GetOwner()->GetActorLocation() + FVector(RelativeForceLocation, 0)) + FVector(RelativeForce.GetRotated(GetOwner()->GetActorRotation().Yaw), 0) * 10, FColor::Emerald, false, -1.0F, 0U, 1);
+		DrawDebugLine(GetWorld(), GetOwner()->GetActorLocation() + FVector(RelativeForceLocation, 0), (GetOwner()->GetActorLocation() + FVector(RelativeForceLocation, 0)) + FVector(RelativeForce.GetRotated(GetOwner()->GetActorRotation().Yaw), 0) * 10, FColor::Emerald, false, -1.0F, 0U, 1);
 
 
 		CenterOfThrust = (1 - VelocityShare) * CenterOfThrust + RelativeForceLocation * VelocityShare;
