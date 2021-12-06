5c676f22c67848fc5b5189fa0f99f16f5c9119b1
Liam Healey <snickerscattoruletheworld@gmail.com>
2021-12-05 17:57:55 -0700

    Comment Most GridMap Functions
    Commented Data Manipulator, Search, Grid State, and some Grid Shape Analysis functions. Also created FilterGridPairs() function. Changed some function signatures, and renamed a typedef.

--- Source/Voidsinger/Voidgrids/GridMap.h
+++ Source/Voidsinger/Voidgrids/GridMap.h
@@ -40,10 +40,10 @@ class TGridMap
 
 private:
 	//The type used for storing a Values and its Location
-	typedef FGridPair<ValueType> ElementType;
+	typedef FGridPair<ValueType> LocationValuePairType;
 
 	//The type used for storing Values and thier Locations
-	typedef TArray<ElementType> GridInfoType;
+	typedef TArray<LocationValuePairType> GridInfoType;
 
 	/* /\    /\ *\
 	\* typedefs */
@@ -63,20 +63,20 @@ private:
 	\* \/        \/ */
 
 public:
-	//Constructs a GridMap with the given dimensions and no Values
+	//Constructs a GridMap with the given dimensions and no Values.
 	TGridMap(GridLocationType GridDimensions = GridLocationType(500))
 	{
 		GridInfo = GridInfoType();
 		GridSize = GridDimensions;
 	}
 
-	//Constructs a GridMap with the given dimensions and Values initilized using the default constuctor at all Locations
+	//Constructs a GridMap with the given dimensions and Values initilized using the default constuctor at all Locations.
 	TGridMap(TSet<GridLocationType> Locations, GridLocationType GridDimensions = GridLocationType(500))
 	{
 		TGridMap(Locations, ValueType(), GridDimensions);
 	}
 
-	//Constructs a GridMap with the given dimensions and creates copies of Value at all Locations
+	//Constructs a GridMap with the given dimensions and creates copies of Value at all Locations.
 	TGridMap(TSet<GridLocationType> ValueLocations, ValueType Value, GridLocationType GridDimensions = GridLocationType(500))
 	{
 		GridSize = GridDimensions;
@@ -87,7 +87,7 @@ public:
 		}
 	}
 
-	//Constucts a GridMap with the given Values and dimensions
+	//Constucts a GridMap with the given Values and dimensions.
 	TGridMap(GridInfoType Values, GridLocationType GridDimensions = GridLocationType(250))
 	{
 		GridInfo = Values.Sort(ComparePair);
@@ -96,28 +96,48 @@ public:
 
 	/* /\        /\ *\
 	\* Constructors */
-	
+	/* Data Manipulator *\
+	\* \/            \/ */
 
-	ValueType Emplace(GridLocationType Location, ValueType Value)
+public:
+	/**
+	 * Constructs a new item at a specified location.
+	 *
+	 * @param Location - The location to add the item at.
+	 * @param Value	- The value of the item
+	 * @return A reference to the newly-inserted item.
+	 */
+	ValueType Emplace(GridLocationType Location, ValueType Value = ValueType())
 	{
 		if (GridInfo.Num() == 0)
 		{
-			return GridInfo.Emplace_GetRef(ElementType(Location, Value)).Value;
+			return GridInfo.Emplace_GetRef(LocationValuePairType(Location, Value)).Value;
 		}
 
-
+		//Uses a bianary search to emplace into a sorted aray
 		int32 InsertionIndex = BinaryInsertionSearch(LocationToRelativeValue(Location));
-
-		GridInfo.EmplaceAt(InsertionIndex, ElementType(Location, Value));
-
-		return GridInfo[InsertionIndex].Value;
+		return GridInfo.EmplaceAt_GetRef(InsertionIndex, LocationValuePairType(Location, Value)).Value;
 	}
 
+
+	//	\/ Remove() \/
+
+	/**
+	 * Removes an item at a specified location.
+	 *
+	 * @param Location - The location to remove the item at.
+	 */
 	void Remove(GridLocationType Location)
 	{
 		int32 RemovalIndex = BinarySearch(LocationToRelativeValue(Location));
 		Remove(RemovalIndex);
 	}
+private:
+	/**
+	 * Removes an item at a specified internal index.
+	 *
+	 * @param Index - The internal index to remove the item at.
+	 */
 	void Remove(int32 Index)
 	{
 		if (GridInfo.IsValidIndex(Index))
@@ -126,42 +146,94 @@ public:
 		}
 	}
 
+	//	/\ Remove() /\
+
+	/* /\            /\ *\
+	\* Data Manipulator */
+	/* Search Functions *\
+	\* \/            \/ */
+
+public:
+	/**
+	 * Checks if there is a value at a given location.
+	 *
+	 * @param Location - The location to seach for.
+	 * @return Whether or not the location has a value.
+	 */
 	bool Contains(GridLocationType Location)
 	{
 		return BinarySearch(LocationToRelativeValue(Location)) >= 0;
 	}
 
-	ValueType FindRef(GridLocationType Location)
-	{
-		int32 Index = BinarySearch(LocationToRelativeValue(Location));
-		if (Index >= 0)
-		{
-			return ValueAtIndex(Index);
-		}
-
-		return ValueType();
-	}
-
+	/**
+	 * Finds the value at a given location.
+	 *
+	 * @param Location - The location to seach at.
+	 * @return The value at Location.
+	 */
 	ValueType* Find(GridLocationType Location)
 	{
 		int32 Index = BinarySearch(LocationToRelativeValue(Location));
 		if (Index >= 0)
 		{
-			return &ValueAtIndex(Index);
+			return &GridInfo[Index].Value;
 		}
 		return nullptr;
 	}
 
-	int32 Num()
+	/**
+	 * Finds the value at a given location.
+	 *
+	 * @param Location - The location to seach at.
+	 * @return A referce to the value at Location.
+	 */
+	ValueType FindRef(GridLocationType Location)
+	{
+		int32 Index = BinarySearch(LocationToRelativeValue(Location));
+		if (Index >= 0)
+		{
+			return GridInfo[Index].Value;
+		}
+
+		return ValueType();
+	}
+
+	/* /\            /\ *\
+	\* Search Functions */
+	/* Grid State Functions *\
+	\* \/			     \/ */
+
+public:	
+	/**
+	 * Returns the number of values in a GridMap.
+	 *
+	 * @return The number of values in a GridMap.
+	 */
+	const int32 Num()
 	{
 		return GridInfo.Num();
 	}
 
+	/**
+	 * Gets all Location Value Pairs.
+	 *
+	 * @return An array of all Location Value Pairs.
+	 */
+	GridInfoType GetGridPairs()
+	{
+		return GridInfo;
+	}
+
+	/**
+	 * Creates an Array containing all Values in the GridMap.
+	 *
+	 * @return An Array containing all Values in the GridMap.
+	 */
 	TArray<ValueType> GenerateValueArray()
 	{
 		TArray<ValueType> Values = TArray<ValueType>();
 
-		for (ElementType GridInfoPair : GridInfo)
+		for (LocationValuePairType GridInfoPair : GridInfo)
 		{
 			Values.Emplace(GridInfoPair.Value);
 		}
@@ -169,11 +241,16 @@ public:
 		return Values;
 	}
 
+	/**
+	 * Creates an Array containing all Locations with values in the GridMap.
+	 *
+	 * @return An Array containing all Locations with values in the GridMap.
+	 */
 	TArray<GridLocationType> GenerateLocationArray()
 	{
 		TArray<GridLocationType> Loactions = TArray<GridLocationType>();
-
-		for (ElementType GridInfoPair : GridInfo)
+		Loactions.Sort
+		for (LocationValuePairType GridInfoPair : GridInfo)
 		{
 			Loactions.Emplace(GridInfoPair.Location);
 		}
@@ -181,63 +258,115 @@ public:
 		return Loactions;
 	}
 
-	GridLocationType& LocationAtIndex(int32 Index)
+	/* /\                /\ *\
+	\* Grid State Functions */
+	/* Grid Shape Analysis *\
+	\* \/			    \/ */
+
+public:
+	/**
+	 * Gets the Location Value Pairs that satisfy the given predicate.
+	 * 
+	 * @param InclusionCondition - If InclusionCondition(LocationValuePairType) is true then the pair will be included in the return value.
+	 * @return An array of Location Value Pairs that satisfy the given predicate.
+	 */
+	template<typename Predicate>
+	TArray<LocationValuePairType> FilterGridPairs(Predicate InclusionCondition)
 	{
-		return GridInfo[Index].Location;
+		TArray<LocationValuePairType> Pairs = TArray<LocationValuePairType>();
+		for (LocationValuePairType GridInfoPair : GridInfo)
+		{
+			if (InclusionCondition(GridInfoPair))
+			{
+				Pairs.Emplace(GridInfoPair.Location);
+			}
+		}
+
+		return Pairs;
 	}
 
-	ValueType& ValueAtIndex(int32 Index)
+	/**
+	 * Checks if StartLocation is connected connected to EndLocation in the grid.
+	 * 
+	 * @param StartLocation - The location in gridspace to begin the connectivty search.
+	 * @param EndLocation -  The target location in gridspace for the connectivty search.
+	 * @return Whether or not StartLocation is connected to EndLocation.
+	 */
+	bool PointsConnected(GridLocationType StartLocation, GridLocationType EndLocation)
 	{
-		return GridInfo[Index].Value;
+		return PointsConnected(StartLocation, EndLocation, AlwaysConnect);
 	}
 
-	bool PointsConnected(GridLocationType StartPoint, GridLocationType EndPoint)
-	{
-		return PointsConnected(StartPoint, EndPoint, AlwaysConnect);
-	}
-	bool PointsConnected(GridLocationType StartPoint, GridLocationType EndPoint, TArray<GridLocationType>& ConnectivityArray)
-	{
-		return PointsConnected(StartPoint, EndPoint, ConnectivityArray, AlwaysConnect);
-	}
-	template<typename BinaryPredicate>
-	bool PointsConnected(GridLocationType StartPoint, GridLocationType EndPoint, BinaryPredicate ConectivityCondition)
+	/**
+	 * Checks if StartLocation is connected connected to EndLocation in the grid by Location Value Pairs that satisfy the given predicate.
+	 *
+	 * @param StartLocation - The location in gridspace to begin the connectivty search.
+	 * @param EndLocation - The target location in gridspace for the connectivty search.
+	 * @param ConectivityCondition - If ConectivityCondition(LocationValuePairType) is false than that location is not considered when searching for valid conections.
+	 * @return Whether or not StartLocation is connected to EndLocation by Location Value Pairs that satisfy the given predicate.
+	 */
+	template<typename Predicate>
+	bool PointsConnected(GridLocationType StartLocation, GridLocationType EndLocation, Predicate ConectivityCondition)
 	{
 		//Initate Conectiveity Array
 		TArray<GridLocationType> ConectivityArray = TArray<GridLocationType>();
-		return PointsConnected(StartPoint, EndPoint, ConectivityArray, ConectivityCondition);
+		return PointsConnected(StartLocation, EndLocation, ConectivityArray, ConectivityCondition);
 	}
-	template<typename BinaryPredicate>
-	bool PointsConnected(GridLocationType StartPoint, GridLocationType EndPoint, TArray<GridLocationType>& ConnectivityArray, BinaryPredicate ConectivityCondition)
-	{
 
+private:
+	/**
+	 * Checks if StartLocation is connected connected to EndLocation in the grid.
+	 *
+	 * @param StartLocation - The location in gridspace to begin the connectivty search.
+	 * @param EndLocation -  The target location in gridspace for the connectivty search.
+	 * @param SeachedLocations - An array storing locations already searched.
+	 * @return Whether or not StartLocation is connected to EndLocation.
+	 */
+	bool PointsConnected(GridLocationType StartLocation, GridLocationType EndLocation, TArray<GridLocationType>& SeachedLocations)
+	{
+		return PointsConnected(StartLocation, EndLocation, SeachedLocations, AlwaysConnect);
+	}
+
+	/**
+	 * Checks if StartLocation is connected connected to EndLocation in the grid.
+	 *
+	 * @param StartLocation - The location in gridspace to begin the connectivty search.
+	 * @param EndLocation -  The target location in gridspace for the connectivty search.
+	 * @param SeachedLocations - An array storing locations already searched.
+	 * @param ConectivityCondition - If ConectivityCondition(LocationValuePairType) is false than that location is not considered when searching for valid conections.
+	 * @return Whether or not StartLocation is connected to EndLocation.
+	 */
+	template<typename Predicate>
+	bool PointsConnected(GridLocationType StartLocation, GridLocationType EndLocation, TArray<GridLocationType>& SeachedLocations, Predicate ConectivityCondition)
+	{
 		//Detect if funtion has reached target
-		if (StartPoint == EndPoint)
+		if (StartLocation == EndLocation)
 		{
 			return true;
 		}
 
 		//Prevent Infinte Loops
-		ConnectivityArray.Emplace(StartPoint);
+		SeachedLocations.Emplace(StartLocation);
 
 		//Initate Variables
 		bool ReturnValue = false;
-		const bool IsXCloser = abs((EndPoint - StartPoint).X) < abs((EndPoint - StartPoint).Y);
-		bool XIsPosive = (EndPoint - StartPoint).X > 0;
-		bool YIsPosive = (EndPoint - StartPoint).Y > 0;
-		//UE_LOG(LogTemp, Warning, TEXT("Direction x=%i, y=%i"), (EndPoint - StartPoint).X, (EndPoint - StartPoint).Y);
+		const bool IsXCloser = abs((EndLocation - StartLocation).X) < abs((EndLocation - StartLocation).Y);
+		bool XIsPosive = (EndLocation - StartLocation).X > 0;
+		bool YIsPosive = (EndLocation - StartLocation).Y > 0;
+		//UE_LOG(LogTemp, Warning, TEXT("Direction x=%i, y=%i"), (EndLocation - StartLocation).X, (EndLocation - StartLocation).Y);
 
 
 		//Iterate though and run recursive function for all adjecent pixels
 		for (int i = 0; i < 4; i++)
 		{
-			//Select next pixel to scan based of of direction to EndPoint
-			GridLocationType TargetPoint = StartPoint + ((!IsXCloser ^ (i % 2 == 1)) ? GridLocationType((XIsPosive ^ (i > 1)) ? 1 : -1, 0) : GridLocationType(0, (YIsPosive ^ (i > 1)) ? 1 : -1));
+			//Select next pixel to scan based of of direction to EndLocation
+			GridLocationType TargetPoint = StartLocation + ((!IsXCloser ^ (i % 2 == 1)) ? GridLocationType((XIsPosive ^ (i > 1)) ? 1 : -1, 0) : GridLocationType(0, (YIsPosive ^ (i > 1)) ? 1 : -1));
 			//UE_LOG(LogTemp, Warning, TEXT("Target Point x=%i, y=%i, Xclose=%i, Xpos=%i, Ypos=%i"), TargetPoint.X, TargetPoint.Y, (IsXCloser ^ (i % 2 == 1)) ? 1 : 0, !(XIsPosive ^ (i > 1)) ? 1 : 0, !(YIsPosive ^ (i > 1)) ? 1 : 0);
 
 			//Scan Pixel
-			if (!ConnectivityArray.Contains(TargetPoint) && Contains(TargetPoint) && ConectivityCondition(FindRef(TargetPoint), TargetPoint))
+			if (!SeachedLocations.Contains(TargetPoint) && Contains(TargetPoint) && ConectivityCondition(FindRef(TargetPoint), TargetPoint))
 			{
-				ReturnValue = PointsConnected(TargetPoint, EndPoint, ConnectivityArray, ConectivityCondition);
+				ReturnValue = PointsConnected(TargetPoint, EndLocation, SeachedLocations, ConectivityCondition);
 				if (ReturnValue)
 				{
 					break;
@@ -249,13 +378,18 @@ public:
 		return ReturnValue;
 	}
 
-	static bool AlwaysConnect(ValueType Value, GridLocationType IntPoint)
+	/**
+	 * Helper Function for PointsConnected. Allways returns true.
+	 * 
+	 * @return true
+	 */
+	static bool AlwaysConnect(LocationValuePairType Value)
 	{
 		return true;
 	}
 
 private:
-	const static bool ComparePair(ElementType& A, ElementType& B)
+	const static bool ComparePair(LocationValuePairType& A, LocationValuePairType& B)
 	{
 		return LocationToRelativeValue(A[0]) < LocationToRelativeValue(B[0]);
 	}
