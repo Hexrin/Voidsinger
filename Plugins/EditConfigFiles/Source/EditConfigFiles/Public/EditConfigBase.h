d70cd1dac0d9b4307de7d1e99b77ac14ac4e481e
Hexrin <71273371+Hexrin@users.noreply.github.com>
2021-12-29 15:19:50 -0700

    setup plugin

---
+++ Plugins/EditConfigFiles/Source/EditConfigFiles/Public/EditConfigBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "EditConfigBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDITCONFIGFILES_API UEditConfigBase : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+};
