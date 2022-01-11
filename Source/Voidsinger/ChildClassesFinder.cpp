d9c4a8f516335afb185a1cb8977ec24e53dc71ef
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-01-11 09:40:37 -0700

    .h should be first included in ChildClassesFinder.cpp

--- Source/Voidsinger/ChildClassesFinder.cpp
+++ Source/Voidsinger/ChildClassesFinder.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "AssetRegistry/AssetRegistryModule.h"
 #include "ChildClassesFinder.h"
+#include "AssetRegistry/AssetRegistryModule.h"
 
 UChildClassesFinder::UChildClassesFinder()
 {
