51659f3a91a90b578a0e32da8ca2376dcfeb2d36
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-01-13 19:38:54 -0700

    liam changed the entire system again

--- Source/Voidsinger/Voidgrids/PartModule.h
+++ Source/Voidsinger/Voidgrids/PartModule.h
@@ -7,13 +7,45 @@
 #include "PartModule.generated.h"
 
 /**
+ * This 
+ */
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum EActivationCue : uint8
+{
+	OnDamaged					UMETA(DisplayName = "OnDamaged", Tooltip = "When the part is damaged in any way")
+	OnRepaired					UMETA(DisplayName = "OnRepaired", Tooltip = "When the part is repaired in any way. This is different from fully repaired as you can repair a part one pixel at a time")
+	OnDestroyed					UMETA(DisplayName = "OnDestroyed", Tooltip = "When the part is completely destroyed")
+	OnFullyRepaired				UMETA(DisplayName = "OnFullyRepaired", Tooltip = "When the part becomes fully intact")
+	OnFunctionalityLost			UMETA(DisplayName = "OnFunctionalityLost", Tooltip = "When the part can no longer function")
+	OnFunctionalityRestored		UMETA(DisplayName = "OnFunctionalityRestored", Tooltip = "When the part gains enough pixels to function")
+	OnVoidsongCue				UMETA(DisplayName = "OnVoidsong", Tooltip = "When a Voidsong cue is called")
+};
+
+ENUM_CLASS_FLAGS(EActivationCue);
+
+/**
  * 
  */
-UCLASS(EditInlineNew, Blueprintable)
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum EVoidsongCue : uint8
+{
+	Beat		UMETA(DisplayName = "Beat")
+	Tick		UMETA(DisplayName="Tick")
+};
+
+ENUM_CLASS_FLAGS(EVoidsongCue)
+
+/**
+ * 
+ */
+UCLASS(EditInlineNew, Blueprintable, HideDropdown)
 class VOIDSINGER_API UPartModule : public UObject
 {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere)
 	float Range;
+
+	UPROPERTY(EditAnywhere)
+
 };
