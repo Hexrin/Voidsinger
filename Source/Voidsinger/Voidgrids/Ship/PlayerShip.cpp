117b90b10a0c54354b162dfe8aa91d98a250e170
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-01-16 08:50:42 -0700

    missed a thing that didn't build on the player

--- Source/Voidsinger/Voidgrids/Ship/PlayerShip.cpp
+++ Source/Voidsinger/Voidgrids/Ship/PlayerShip.cpp
@@ -4,3 +4,12 @@ void APlayerShip::SetCameraZoom(float Percent)
 {
 	//CameraHeight = (CameraMaxHeight - CameraMinHeight)FMath::Pow(Percent, CameraZoomPower) + CameraMinHeight;
 }
+
+float APlayerShip::GetCameraZoom() const
+{
+	return 0.0f;
+}
+
+void APlayerShip::ZoomAxisInput(float AxisValue)
+{
+}
