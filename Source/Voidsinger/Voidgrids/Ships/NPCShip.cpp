30598334752a520e114af376211de66e4f528a6c
Liam Healey <snickerscattoruletheworld@gmail.com>
2022-01-16 11:40:33 -0700

    Fix Chrashes

--- Source/Voidsinger/Voidgrids/Ships/NPCShip.cpp
+++ Source/Voidsinger/Voidgrids/Ships/NPCShip.cpp
@@ -3,5 +3,24 @@
 
 ANPCShip::ANPCShip()
 {
+	if (IsValid(ShipTypeData))
+	{
+		SetPixelMold(ShipTypeData->Mold);
+	}
+}
 
+/**
+ * Gets the faction of the Voidgrid.
+ *
+ * @return The faction of the Voidgrid.
+ */
+EFaction ANPCShip::GetFaction() const
+{
+	if (IsValid(ShipTypeData))
+	{
+		return ShipTypeData->Faction;
+	}
+
+	//Return default faction.
+	return (EFaction)0;
 }
