b9d4e92eef9ed07e8831edaf9281f6c5e68b0c52
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-01-15 17:33:34 -0700

    renamed UPartFunction back to UPartModule

--- Source/Voidsinger/Voidgrids/PartData.h
+++ Source/Voidsinger/Voidgrids/PartData.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PartFunction.h"
+#include "PartModule.h"
 #include "Engine/DataAsset.h"
 #include "PartData.generated.h"
 
@@ -34,7 +34,7 @@ public:
 
 	//Stores the ways that this part can be activated.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced)
-	TArray<UPartFunction*> Modules;
+	TArray<UPartModule*> Modules;
 
 	//Stores the cost to place the part in Pixels.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
