fde15abb982ad2f469b31525d249048135143c0a
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-01-15 17:25:06 -0700

    Moved activate functions to a subclass of PartModule

---
+++ Source/Voidsinger/Voidgrids/ActivatablePartModule.cpp
@@ -0,0 +1,149 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Voidsinger/Voidgrids/ActivatablePartModule.h"
+#include "Part.h"
+#include "Voidgrid.h"
+
+/* -------------------- *\
+\* \/ Initialization \/ */
+
+/**
+ * Initializes the part module's protected and private variables. Overriden from PartModule to call BindToDelegates().
+ *
+ * @param OwningPart - The part that this module is being initialized from
+ */
+void UActivatablePartModule::InitializeVariables(UPart* OwningPart)
+{
+	Super::InitializeVariables(OwningPart);
+
+	BindToDelegates();
+}
+
+/* /\ Initialization /\ *\
+\* -------------------- */
+
+/* ---------------- *\
+\* \/ Activation \/ */
+
+// \/ Activate \/
+
+/**
+ * Calls the "OnActivate" function with an Effectiveness of 1 so the part module's functionality is executed.
+ */
+void UActivatablePartModule::Activate()
+{
+	TArray<TSubclassOf<UBaseVerbVoidsong>> EmptyVerbArray;
+	OnActivate(EmptyVerbArray, 1);
+}
+
+/**
+ * Calls the "OnActivate" function so the part module's functionality is executed
+ *
+ * @param Effectiveness - The effectiveness of the activation. Useful for when activate is called every tick
+ */
+void UActivatablePartModule::ActivateWithEffectiveness(float Effectiveness)
+{
+	TArray<TSubclassOf<UBaseVerbVoidsong>> EmptyVerbArray;
+	OnActivate(EmptyVerbArray, Effectiveness);
+}
+
+/**
+ * Activate overload - Checks whether "OnActivate" should be called by seeing if this module statisfies the Voidsong conditions. If it does, it calls the "OnActivate" function so the part module's functionality is executed
+ *
+ * @param Factions - The Factions that were activated
+ * @param Nouns - The Nouns that were activated
+ * @param Verbs - The Verbs that were activated
+ * @param Effectiveness - The effectiveness of the activation. Useful for when activate is called every tick
+ */
+void UActivatablePartModule::ActivateFromVoidsong(const TArray<EFaction>& Factions, const TArray<ENoun>& Nouns, const TArray<TSubclassOf<UBaseVerbVoidsong>>& Verbs, const TArray<TSubclassOf<UBaseVoidsong>>& PlayableVoidsongs, float Effectiveness)
+{
+	//If this module's noun is not unbound, check if it satisfies the Voidsong requirements to activate
+	if (Noun != ENoun::Unbound)
+	{
+
+		//((Factions.IsEmpty() && AvailableFactions.Contains(Cast<ABaseShip>(GetOuter()->GetOuter())->GetFaction())) != Factions.Contains(Cast<ABaseShip>(GetOuter()->GetOuter())->GetFaction())) && NounsCheck
+
+		// \/ Finds playable Factions  and Nouns from all playable Voidsongs \/
+
+		TArray<EFaction> PlayableFactions;
+		TArray<ENoun> PlayableNouns;
+
+		for (TSubclassOf<UBaseVoidsong> EachPlayableVoidsong : PlayableVoidsongs)
+		{
+			if (EachPlayableVoidsong->IsChildOf(UBaseFactionVoidsong::StaticClass()))
+			{
+				PlayableFactions.Emplace(Cast<UBaseFactionVoidsong>(EachPlayableVoidsong->GetDefaultObject())->Faction);
+			}
+			if (EachPlayableVoidsong->IsChildOf(UBaseNounVoidsong::StaticClass()))
+			{
+				PlayableNouns.Emplace(Cast<UBaseNounVoidsong>(EachPlayableVoidsong->GetDefaultObject())->Noun);
+			}
+		}
+
+		// /\ Finds playable Factions and Nouns from all playable Voidsongs /\
+
+		//Checks if this module's faction (referred to as "this faction") is playable
+		bool bFactionIsPlayable = PlayableFactions.Contains(Part->GetVoidgrid()->GetFaction());
+
+		//Factions check is true if 
+		//	      |- No factions played, but this faction is playable -| or |--- This faction is one of the factions played ---|
+		bool bFactionsCheck = (Factions.IsEmpty() && bFactionIsPlayable) || Factions.Contains(Part->GetVoidgrid()->GetFaction());
+
+		//Checks is this module's noun (referred to as "this noun") is playable
+		bool bNounIsPlayable = PlayableNouns.Contains(Noun);
+
+		//Nouns check is true if
+		//	    |- No noun played, but this noun is playable -| or |- This noun is one of the nouns played -|
+		bool bNounsCheck = (Nouns.IsEmpty() && bNounIsPlayable) || Nouns.Contains(Noun);
+
+		//If this part module satisfies the conditions of the Voidsong, call OnActivate()
+		if (bFactionsCheck && bNounsCheck)
+		{
+			OnActivate(Verbs, Effectiveness);
+		}
+	}
+}
+
+// /\ Activate /\
+
+/* /\ Activation /\ *\
+\* ---------------- */
+
+/* ---------------------- *\
+\* \/ Delegate Binding \/ */
+
+void UActivatablePartModule::BindToDelegates()
+{
+	if ((bool)(ActivationCues & EActivationCue::OnDamaged))
+	{
+		Part->OnDamaged.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnRepaired))
+	{
+		Part->OnRepaired.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnFunctionalityLost))
+	{
+		Part->OnFunctionalityLost.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnFunctionalityRestored))
+	{
+		Part->OnFunctionalityRestored.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnDestroyed))
+	{
+		Part->OnDestroyed.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnFullyRepaired))
+	{
+		Part->OnFullyRepaired.AddDynamic(this, &UActivatablePartModule::Activate);
+	}
+	if ((bool)(ActivationCues & EActivationCue::OnTick))
+	{
+
+	}
+}
+
+/* /\ Delegate Binding /\ *\
+\* ---------------------- */
---
+++ Source/Voidsinger/Voidgrids/ActivatablePartModule.h
@@ -0,0 +1,159 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Voidsinger/Voidgrids/PartModule.h"
+#include "ActivatablePartModule.generated.h"
+
+
+/* -------------------------- *\
+\* \/ Forward Declarations \/ */
+
+class UPart;
+
+/* /\ Foward Declarations /\ *\
+\* ------------------------- */
+
+/* \/ ============== \/ *\
+|  \/ EActivationCue \/  |
+\* \/ ============== \/ */
+
+/**
+ * This enum stores delegate names. Module functions can be bound to these delegates by choosing options in this enum.
+ */
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class EActivationCue : uint8
+{
+	OnDamaged					UMETA(DisplayName = "OnDamaged", Tooltip = "When the part is damaged in any way"),
+	OnRepaired					UMETA(DisplayName = "OnRepaired", Tooltip = "When the part is repaired in any way. This is different from fully repaired as you can repair a part one pixel at a time"),
+	OnFunctionalityLost			UMETA(DisplayName = "OnFunctionalityLost", Tooltip = "When the part can no longer function"),
+	OnFunctionalityRestored		UMETA(DisplayName = "OnFunctionalityRestored", Tooltip = "When the part gains enough pixels to function"),
+	OnDestroyed					UMETA(DisplayName = "OnDestroyed", Tooltip = "When the part is completely destroyed"),
+	OnFullyRepaired				UMETA(DisplayName = "OnFullyRepaired", Tooltip = "When the part becomes fully intact"),
+	OnTick						UMETA(DisplayName = "OnTick", Tooltip = "Every tick"),
+	OnVoidsongCue				UMETA(DisplayName = "OnVoidsong", Tooltip = "When a Voidsong cue is called"),
+};
+ENUM_CLASS_FLAGS(EActivationCue);
+
+/* /\ ============== /\ *\
+|  /\ EActivationCue /\  |
+\* /\ ============== /\ */
+
+/* \/ ============ \/ *\
+|  \/ EVoidsongCue \/  |
+\* \/ ============ \/ */
+
+/**
+ * This enum stores delegate names specific to Voidsongs. Module functions can be bound to these delegates by choosing options in this enum.
+ */
+UENUM(BlueprintType, meta = (Bitflags, UseEnumValuesAsMaskValuesInEditor = "true"))
+enum class EVoidsongCue : uint8
+{
+	OnBeat			UMETA(DisplayName = "OnBeat"),
+	ForDuration		UMETA(DisplayName = "ForDuration"),
+};
+ENUM_CLASS_FLAGS(EVoidsongCue)
+
+/* /\ ============ /\ *\
+|  /\ EVoidsongCue /\  |
+\* /\ ============ /\ */
+
+/* \/ ====================== \/ *\
+|  \/ UActivatablePartModule \/  |
+\* \/ ====================== \/ */
+
+/**
+ * This subclass of UPartModule is activatable. Functionality is defined in the "OnActivate" BlueprintImplementable function. 
+ * The "Activate" function is bound to whatever delegates are selected in the EActivationCue and EVoidsongCue enums.
+ */
+UCLASS()
+class VOIDSINGER_API UActivatablePartModule : public UPartModule
+{
+	GENERATED_BODY()
+	
+
+	/* -------------------- *\
+	\* \/ Initialization \/ */
+
+public:
+
+	/**
+	 * Initializes the part module's protected and private variables. Overriden from PartModule to call BindToDelegates().
+	 *
+	 * @param OwningPart - The part that this module is being initialized from
+	 */
+	void InitializeVariables(UPart* OwningPart);
+
+	/* /\ Initialization /\ *\
+	\* -------------------- */
+
+	/* ---------------- *\
+	\* \/ Activation \/ */
+
+public:
+
+	/**
+	 * Allows for blueprint logic when the part module is activated
+	 */
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = "Activate", Category = "Activation")
+		void OnActivate(const TArray<TSubclassOf<UBaseVerbVoidsong>>& Verbs, float Effectiveness);
+
+	// \/ Activate \/
+
+	/**
+	 * Calls the "OnActivate" function with an Effectiveness of 1 so the part module's functionality is executed.
+	 */
+	void Activate();
+
+	/**
+	 * Calls the "OnActivate" function so the part module's functionality is executed
+	 *
+	 * @param Effectiveness - The effectiveness of the activation. Useful for when activate is called every tick
+	 */
+	UFUNCTION(BlueprintCallable, DisplayName = "Activate", Category = "Activation")
+	void ActivateWithEffectiveness(float Effectiveness);
+
+	/**
+	 * Checks whether "OnActivate" should be called by seeing if this module statisfies the Voidsong conditions. If it does, it calls the "OnActivate" function so the part module's functionality is executed
+	 *
+	 * @param Factions - The Factions that were activated
+	 * @param Nouns - The Nouns that were activated
+	 * @param Verbs - The Verbs that were activated
+	 * @param Effectiveness - The effectiveness of the activation. Useful for when activate is called every tick
+	 */
+	void ActivateFromVoidsong(const TArray<EFaction>& Factions, const TArray<ENoun>& Nouns, const TArray<TSubclassOf<UBaseVerbVoidsong>>& Verbs, const TArray<TSubclassOf<UBaseVoidsong>>& PlayableVoidsongs, float Effectiveness = 1);
+
+	// /\ Activate /\
+
+	//Stores what Noun Voidsong will activate this part module. If unbound, this module will not be activatable by a Voidsong.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Activation")
+	ENoun Noun;
+
+	/* /\ Activation /\ *\
+	\* ---------------- */
+
+	/* ---------------------- *\
+	\* \/ Delegate Binding \/ */
+
+	/**
+	 * Binds the activate function to all selected delegates
+	 */
+	void BindToDelegates();
+
+	// The events to bind Activate to
+	UPROPERTY(EditAnywhere, Category = "DelegateBinding")
+	EActivationCue ActivationCues;
+
+	// The Voidsong events to bind Activate to
+	UPROPERTY(EditAnywhere, Category = "DelegateBinding")
+	EVoidsongCue VoidsongCues;
+
+	/* /\ Delegate Binding /\ *\
+	\* ---------------------- */
+
+};
+
+/* /\ =========== /\ *\
+|  /\ UPartModule /\  |
+\* /\ =========== /\ */
