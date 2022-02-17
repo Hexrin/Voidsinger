60693dd4d8dac5dfcbbd9ca550ab0e40ce1541f9
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-17 10:15:41 -0700

    Merge remote-tracking branch 'origin/AI-Creation' into refactor-master-merging

---
+++ Source/Voidsinger/Parts/PartTooltipData.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/Texture.h"
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PartTooltipData.generated.h"
+
+/**
+ * Stores information realating to part tooltips.
+ */
+UCLASS()
+class VOIDSINGER_API UPartTooltipData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	//The name of the Part
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText Name;
+
+	//Describes what the part does and its stats.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText Description;
+
+	//The image used to show the conectivity points of this part.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* ConectivityPointImage = nullptr;
+};
