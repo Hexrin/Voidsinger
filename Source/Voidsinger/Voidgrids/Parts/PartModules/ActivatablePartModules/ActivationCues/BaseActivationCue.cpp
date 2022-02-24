0b18bbe6b6a46269facc1203603bafaa2b8a241a
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-24 09:27:46 -0700

    Created the base activation cue

---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h"
+
+/*
+ * Initializes this activation cue's variables and bindings
+ */
+void UBaseActivationCue::Initialize(UPart* OwningPart)
+{
+}
---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/PartActivationData.h"
+#include "BaseActivationCue.generated.h"
+
+// \/ Forward declarations \/ /
+
+class UPart;
+
+// /\ Forward declarations /\ /
+
+//Broadcasts the activation data when this activation cue is called
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActivationCue, FPartActivationData, ActivationData);
+
+/**
+ * Base class for activation cues. Activation cues contain delegates that part modules can bind to.
+ */
+UCLASS()
+class VOIDSINGER_API UBaseActivationCue : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+
+	/*
+	 * Initializes this activation cue's variables and bindings
+	 */
+	virtual void Initialize(UPart* OwningPart);
+
+	//Delegate for broadcasting the activation data when this activation cue is called
+	UPROPERTY(BlueprintAssignable)
+	FActivationCue OnActivationCue;
+
+};
