5b0ba65c5141efcff2d10d15ce25c093648dbc77
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-25 08:01:25 -0700

    Finished up the thrust request cue

--- Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.cpp
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.cpp
@@ -1,5 +1,37 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h"
+#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h"
+#include "Voidsinger/Voidgrids/Parts/Part.h"
+#include "Voidsinger/Voidgrids/Voidgrid.h"
+#include "Voidsinger/Voidgrids/Parts/PartModules/ThrustManager.h"
 
+/* -------------------- *\
+\* \/ Initialization \/ */
+
+/*
+ * Initializes this activation cue's variables and bindings
+ */
+void UThrustRequestCue::Initialize(UPart* OwningPart)
+{
+	OwningPart->GetVoidgrid()->ThrustManager->OnThrustRequest.AddDynamic(this, &UThrustRequestCue::BroadcastDelegate);
+}
+
+/* /\ Initialization /\ *\
+\* -------------------- */
+
+/* --------------- *\
+\* \/ Delegates \/ */
+
+/*
+ * Broadcasts the "OnActivate" delegate
+ *
+ * @param Data - The activation data containing the effectiveness, move direction, and rotation of the thrust request
+ */
+void UThrustRequestCue::BroadcastDelegate(FPartActivationData Data)
+{
+	OnActivate.Broadcast(Data);
+}
+
+/* /\ Delegates /\ *\
+\* --------------- */
--- Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h"
+#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h"
 #include "ThrustRequestCue.generated.h"
 
 /**
@@ -14,4 +14,32 @@ class VOIDSINGER_API UThrustRequestCue : public UBaseActivationCue
 {
 	GENERATED_BODY()
 	
+	/* -------------------- *\
+	\* \/ Initialization \/ */
+
+public:
+
+	/*
+	 * Initializes this activation cue's variables and bindings
+	 */
+	virtual void Initialize(UPart* OwningPart) override;
+
+	/* /\ Initialization /\ *\
+	\* -------------------- */
+
+	/* --------------- *\
+	\* \/ Delegates \/ */
+
+private:
+
+	/*
+	 * Broadcasts the "OnActivate" delegate
+	 *
+	 * @param Data - The activation data containing the effectiveness, move direction, and rotation of the thrust request
+	 */
+	UFUNCTION()
+	void BroadcastDelegate(FPartActivationData Data);
+
+	/* /\ Delegates /\ *\
+	\* --------------- */
 };
