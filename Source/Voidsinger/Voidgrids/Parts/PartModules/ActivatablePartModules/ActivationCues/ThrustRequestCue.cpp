8cf0c48afdd4cc79ff15f136c329e1ef3f06f37b
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-24 23:10:40 -0700

    Created thrust request cue class

---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h"
+
---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ThrustRequestCue.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h"
+#include "ThrustRequestCue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOIDSINGER_API UThrustRequestCue : public UBaseActivationCue
+{
+	GENERATED_BODY()
+	
+};
