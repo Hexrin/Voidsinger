9015e7f3daa0edbdb91466d80266e4f855054d72
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-25 08:39:37 -0700

    Created the resource cue
    It can't be finished without functions on the resource-systems branch.

---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ResourceCue.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseActivationCue.h"
+#include "ResourceCue.h"
+
+/* -------------------- *\
+\* \/ Initialization \/ */
+
+/*
+ * Initializes this activation cue's variables and bindings
+ */
+void UResourceCue::Initialize(UPart* OwningPart)
+{
+	for (UBaseActivationCue* EachActivationCue : ActivationCues)
+	{
+		EachActivationCue->Initialize(OwningPart);
+		EachActivationCue->OnActivate.AddUniqueDynamic(this, &UResourceCue::ManageResources);
+	}
+}
+
+/* /\ Initialization /\ *\
+\* -------------------- */
+
+/* --------------- *\
+\* \/ Delegates \/ */
+
+/*
+ * Creates and uses the resources specified
+ *
+ * @param Data - The activation data containing all relavent information, including the effectiveness
+ */
+void UResourceCue::ManageResources(FPartActivationData Data)
+{
+
+}
+
+/* /\ Delegates /\ *\
+\* --------------- */
---
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/ResourceCue.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ActivationCues/BaseActivationCue.h"
+#include "Voidsinger/VoidsingerTypes.h"
+#include "ResourceCue.generated.h"
+
+// \/ Forward declarations \/ /
+
+class AVoidgrid;
+
+// /\ Forward declarations /\ /
+
+/* \/ ============ \/ *\
+|  \/ UResourceCue \/  |
+\* \/ ============ \/ */
+
+/**
+ * Subclass of the base activation cue that is able to create and use resources
+ */
+UCLASS()
+class VOIDSINGER_API UResourceCue : public UBaseActivationCue
+{
+	GENERATED_BODY()
+	
+	/* -------------------- *\
+	\* \/ Initialization \/ */
+
+public:
+
+	/*
+	 * Initializes this activation cue's variables and bindings
+	 */
+	virtual void Initialize(UPart* OwningPart) override;
+
+	/* /\ Initialization /\ *\
+	\* -------------------- */
+
+	/* --------------- *\
+	\* \/ Delegates \/ */
+
+public:
+
+	//Stores when this activation cue will be activated
+	UPROPERTY(Instanced, EditAnywhere)
+	TArray<UBaseActivationCue*> ActivationCues;
+
+private:
+
+	/*
+	 * Creates and uses the resources specified
+	 *
+	 * @param Data - The activation data containing all relavent information, including the effectiveness
+	 */
+	UFUNCTION()
+	void ManageResources(FPartActivationData Data);
+
+	/* /\ Delegates /\ *\
+	\* --------------- */
+
+	/* --------------- *\
+	\* \/ Resources \/ */
+
+public:
+
+	//Stores what resources this cue will use
+	UPROPERTY(EditAnywhere)
+	TMap<EResourceType, float> ResourcesToUse;
+
+	//Stores what resources this cue will create
+	UPROPERTY(EditAnywhere)
+	TMap<EResourceType, float> ResourcesToCreate;
+
+private:
+
+	AVoidgrid* Voidgrid;
+
+	/* /\ Resources /\ *\
+	\* --------------- */
+
+};
+
+/* /\ ============ /\ *\
+|  /\ UResourceCue /\  |
+\* /\ ============ /\ */
