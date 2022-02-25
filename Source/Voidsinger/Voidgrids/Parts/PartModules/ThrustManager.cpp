171dfcb87d53cbcb2afc90317168bd0a7d5183e3
Hexrin <71273371+Hexrin@users.noreply.github.com>
2022-02-25 08:01:06 -0700

    Added a thrust request delegate on the thrust manager

--- Source/Voidsinger/Voidgrids/Parts/PartModules/ThrustManager.cpp
+++ Source/Voidsinger/Voidgrids/Parts/PartModules/ThrustManager.cpp
@@ -3,7 +3,7 @@
 
 #include "ThrustManager.h"
 
-#include "Voidsinger/Voidgrids/Parts/PartModules/ActivatablePartModules/ThrusterModule.h"
+#include "ThrusterModule.h"
 #include "Voidsinger/Voidgrids/Voidgrid.h"
 
 UThrustManager::UThrustManager()
